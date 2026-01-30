bcc67982bf2b9d686c0defc1f74daa61dbdbb3af
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-01-31 00:48:29 +0530

    Time: 0 ms (100%), Space: 48.3 MB (48.4%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void reverseString(char[] s) {
+        // s = s.toLowerCase().replaceAll( "");
+
+        int i = 0;
+        int j= s.length-1;
+
+        while(i<=j) {
+
+            char temp = s[i];
+            
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+
+        }
+
+        // return i;
+    }
+}
