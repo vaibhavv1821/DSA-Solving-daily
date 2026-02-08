97c1751c730509a8cf53eefb72724ec07ae74c2d
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-02-08 23:25:42 +0530

    Time: 8 ms (48.13%), Space: 46.6 MB (51.18%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -1,28 +1,20 @@
 class Solution {
     public boolean divideArray(int[] arr) {
-        if(arr.length %2!= 0) {
+        if (arr.length % 2 != 0) {
             return false;
         }
-        HashMap <Integer, Integer> set = new HashMap <> ();
-        for (int i=0; i<arr.length; i++) {
-            set.put(arr[i] , set.getOrDefault(arr[i] , 0) +1);
+        HashMap<Integer, Integer> set = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            set.put(arr[i], set.getOrDefault(arr[i], 0) + 1);
         }
 
-        for (int i=0; i< arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (set.get(arr[i]) % 2 != 0) {
                 return false;
             }
 
         }
         return true;
-    }
 
-    static {
-Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-    try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-            fw.write("0");
-        } catch (Exception e) {
-        }
-    }));
-}
+    }
 }
