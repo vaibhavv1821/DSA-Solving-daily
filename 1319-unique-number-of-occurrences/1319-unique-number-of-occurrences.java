c2f40801b91e4fa84377a22c6588c67a7a3cbf44
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-02-15 23:33:17 +0530

    Time: 2 ms (98.3%), Space: 44 MB (17.02%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+
+        // Step 1: Count frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Step 2: Check uniqueness
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int freq : map.values()) {
+
+            // If already present, not unique
+            if (set.contains(freq)) {
+                return false;
+            }
+
+            set.add(freq);
+        }
+
+        return true;
+    }
+}
