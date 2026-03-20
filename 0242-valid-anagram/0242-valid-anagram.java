b06fb343e0c3043b746e9e32d3215403d890ae31
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-03-21 01:02:13 +0530

    Time: 27 ms (5.51%), Space: 46.5 MB (30.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,39 +1,21 @@
 import java.util.HashMap;
 
 class Solution {
-
     public boolean isAnagram(String s, String t) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        t = t.toLowerCase().replaceAll("[^a-z0-9]", "");
 
-        // Step 1: Length check
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        // Step 2: Create frequency map
         HashMap<Character, Integer> map = new HashMap<>();
 
-        // Step 3: Count characters in s
-        for (char ch : s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
+        for (char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
 
-        // Step 4: Reduce count using t
-        for (char ch : t.toCharArray()) {
+        for (char d : t.toCharArray())
+            map.put(d, map.getOrDefault(d, 0) - 1);
 
-            // If character not present → not anagram
-            if (!map.containsKey(ch)) {
-                return false;
-            }
+        for (int count : map.values())
+            if (count != 0) return false;
 
-            map.put(ch, map.get(ch) - 1);
-
-            // Remove if count becomes 0
-            if (map.get(ch) == 0) {
-                map.remove(ch);
-            }
-        }
-
-        // Step 5: If map empty → anagram
-        return map.isEmpty();
+        return true;
     }
 }
