e73f8db3f71f83eeda3a093d71c9e7baab8574fa
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-04-21 23:14:35 +0530

    Time: 32 ms (5.11%), Space: 4 MB (38.59%) - LeetHub

--- 0195-tenth-line/0195-tenth-line.sh
+++ 0195-tenth-line/0195-tenth-line.sh
@@ -1,25 +1,20 @@
-import java.io.*;
+# Solution 1
+cnt=0
+while read line && [ $cnt -le 10 ]; do
+  let 'cnt = cnt + 1'
+  if [ $cnt -eq 10 ]; then
+    echo $line
+    exit 0
+  fi
+done < file.txt
 
-public class TenthLine {
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader("file.txt"))) {
-            String line;
-            int count = 0;
+# Solution 2
+awk 'FNR == 10 {print }'  file.txt
+# OR
+awk 'NR == 10' file.txt
 
-            while ((line = br.readLine()) != null) {
-                count++;
-                if (count == 10) {
-                    System.out.println(line);
-                    break;
-                }
-            }
+# Solution 3
+sed -n 10p file.txt
 
-            if (count < 10) {
-                System.out.println("File has less than 10 lines.");
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+# Solution 4
+tail -n+10 file.txt|head -1
