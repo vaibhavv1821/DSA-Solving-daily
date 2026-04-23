181b684f48324e72047902a4888fe7262fb6c14a
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-04-23 23:03:07 +0530

    Time: 1 ms (64.29%), Space: 43 MB (73.29%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -3,10 +3,9 @@ class Solution {
         int count =0;
 
         for (int i=0; i<jewels.length(); i++){
-            char ch = jewels.charAt(i);
             
             for(int j = 0 ; j < stones.length();j++){
-                if(ch == stones.charAt(j)){
+                if(jewels.charAt(i) == stones.charAt(j)){
                     count++;
                 }
             }
