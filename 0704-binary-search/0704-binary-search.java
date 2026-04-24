edae6f53d1d7241818497e541632c8e454438377
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-04-24 17:14:42 +0530

    Time: 1 ms (8.99%), Space: 48.3 MB (71.43%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        for (int i=0;i<arr.length;i++){
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
