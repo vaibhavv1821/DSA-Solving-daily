e11b9b6ba5eecf7a94ace0d2b9b48df6b649cc3a
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-04-27 15:44:27 +0530

    Time: 19 ms (11.75%), Space: 47.4 MB (6.45%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public List<String> commonChars(String[] words) {
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        // first word frequency
+        for(char c : words[0].toCharArray()){
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+
+        // compare with remaining words
+        for(int i=1; i<words.length; i++){
+
+            HashMap<Character,Integer> temp = new HashMap<>();
+
+            for(char c : words[i].toCharArray()){
+                temp.put(c, temp.getOrDefault(c,0)+1);
+            }
+
+            // keep minimum frequency
+            for(char ch : new HashSet<>(map.keySet())){
+                if(temp.containsKey(ch)){
+                    map.put(ch, Math.min(map.get(ch), temp.get(ch)));
+                } else{
+                    map.remove(ch);
+                }
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+
+        for(char ch : map.keySet()){
+            int count = map.get(ch);
+            while(count-- > 0){
+                ans.add(String.valueOf(ch));
+            }
+        }
+
+        return ans;
+    }
+}
