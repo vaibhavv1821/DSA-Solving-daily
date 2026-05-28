ee84215f95a0cae36f23b1338d702d18020e80a1
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-05-28 20:22:19 +0530

    .java Medium 12 ms (99.68%) 05-28-2026

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minSwaps(String s) {
+        int count=0;
+        for(char c:s.toCharArray()){
+            if(c=='['){
+                count++;
+            }else{
+                if(count>0){
+                    count--;
+                }
+            }
+        }
+        return (count+1)/2;
+        
+    }
+}
