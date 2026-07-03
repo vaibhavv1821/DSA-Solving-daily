a4863d378469a1a8edb9e975683889c641ef50eb
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-03 22:54:41 +0530

    .java Easy 0 ms (100%) 07-03-2026

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            if (!Character.isLetter(arr[left])) {
+                left++;
+            } else if (!Character.isLetter(arr[right])) {
+                right--;
+            } else {
+                // swap letters
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(arr);
+    }
+}
