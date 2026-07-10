2cd6869df34a3846549396022c018ed62336bd6a
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-10 18:53:19 +0530

    Time: 1 ms (97.65%), Space: 42.6 MB (42.46%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        int right =x;
+        int left = 1;
+        int res =0;
+        if (x == 0 || x == 1) return x;
+        while(left<=right){
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                res = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+           
+        }
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
