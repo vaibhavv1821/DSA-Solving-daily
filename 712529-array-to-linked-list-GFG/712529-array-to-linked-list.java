d135a3ad53a1e5d00fa6bc221698aa7bbd17a2e7
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-12 15:06:01 +0530

    Added solution - LeetHub

---
+++ 712529-array-to-linked-list-GFG/712529-array-to-linked-list.java
@@ -0,0 +1,20 @@
+class Solution {
+    public Node arrayToList(int arr[]) {
+        // code here
+        if(arr.length==0){
+            return null;
+        }
+        Node start=new Node(arr[0]);
+        Node curr=start;
+        for(int i=1;i<arr.length;i++)
+        {
+            curr.next=new Node(arr[i]);
+            curr=curr.next;
+        }
+        return start;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
