83e6ca330a6542466b8f2f6e1af6ccc37c9c3829
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-14 22:23:34 +0530

    Added solution - Time: O, Space: O(?)

---
+++ 910057-doubly-linked-list-from-an-array-GFG/910057-doubly-linked-list-from-an-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public Node createDLL(int arr[]) {
+
+        if (arr.length == 0)
+            return null;
+
+        Node head = new Node(arr[0]);
+        Node curr = head;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            Node newNode = new Node(arr[i]);
+
+            curr.next = newNode;
+            newNode.prev = curr;
+
+            curr = newNode;
+        }
+
+        return head;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
