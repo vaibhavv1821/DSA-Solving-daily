3f1709ea3fdcf4a68cca6c8f95ecbb257e45e394
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-16 00:02:29 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 703913--GFG/703913-.java
@@ -0,0 +1,13 @@
+class Solution {
+    int factorial(int n) {
+        if(n>=1){
+            return n*factorial(n-1);
+        }
+        else
+        return 1;
+    }
+       }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
