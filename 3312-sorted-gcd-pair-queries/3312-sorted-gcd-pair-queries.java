8d4d4243bf94587f2c3215fa6d9819ed2b74f7ad
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-17 23:02:36 +0530

    Time: 28 ms (96.88%), Space: 102.5 MB (40.63%) - LeetHub - Time: O(n + M log M + q log M), Space: O(M + q)

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int max = 0;
+        for (int x : nums) max = Math.max(max, x);
+
+        int[] freq = new int[max + 1];
+        for (int x : nums) freq[x]++;
+
+        long[] exact = new long[max + 1];
+
+        for (int g = max; g >= 1; g--) {
+            long cnt = 0;
+
+            for (int multiple = g; multiple <= max; multiple += g) {
+                cnt += freq[multiple];
+            }
+
+            long pairs = cnt * (cnt - 1) / 2;
+
+            for (int multiple = g * 2; multiple <= max; multiple += g) {
+                pairs -= exact[multiple];
+            }
+
+            exact[g] = pairs;
+        }
+
+        long[] prefix = new long[max + 1];
+        for (int i = 1; i <= max; i++) {
+            prefix[i] = prefix[i - 1] + exact[i];
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            long target = queries[i] + 1;
+
+            int l = 1, r = max;
+            while (l < r) {
+                int mid = (l + r) / 2;
+
+                if (prefix[mid] >= target)
+                    r = mid;
+                else
+                    l = mid + 1;
+            }
+
+            ans[i] = l;
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
