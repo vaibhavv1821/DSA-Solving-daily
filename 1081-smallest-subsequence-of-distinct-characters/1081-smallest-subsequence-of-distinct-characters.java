42bcd6d81914120d28529d294c73d51c1eb70a74
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-19 23:23:22 +0530

    Time: 2 ms (98.45%), Space: 42.9 MB (65.77%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String smallestSubsequence(String text) {
+        StringBuilder sb = new StringBuilder();
+
+        int[] count = new int[128];
+        boolean[] used = new boolean[128];
+
+        // Count frequency of each character
+        for (char c : text.toCharArray()) {
+            count[c]++;
+        }
+
+        for (char c : text.toCharArray()) {
+            count[c]--;
+
+            // Skip if already included
+            if (used[c]) {
+                continue;
+            }
+
+            // Maintain lexicographically smallest order
+            while (sb.length() > 0 &&
+                   last(sb) > c &&
+                   count[last(sb)] > 0) {
+
+                used[last(sb)] = false;
+                sb.setLength(sb.length() - 1);
+            }
+
+            sb.append(c);
+            used[c] = true;
+        }
+
+        return sb.toString();
+    }
+
+    private char last(StringBuilder sb) {
+        return sb.charAt(sb.length() - 1);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
