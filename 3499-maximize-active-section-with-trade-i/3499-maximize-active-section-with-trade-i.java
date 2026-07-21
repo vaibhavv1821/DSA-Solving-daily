17d9ee8c0d4b2e59444e1cf137f23a2516681ed8
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-21 23:04:39 +0530

    Time: 168 ms (14.08%), Space: 51.1 MB (12.68%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        int ones = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') ones++;
+        }
+
+        String t = "1" + s + "1";
+
+        ArrayList<Character> type = new ArrayList<>();
+        ArrayList<Integer> len = new ArrayList<>();
+
+        for (char c : t.toCharArray()) {
+            if (type.isEmpty() || type.get(type.size() - 1) != c) {
+                type.add(c);
+                len.add(1);
+            } else {
+                len.set(len.size() - 1, len.get(len.size() - 1) + 1);
+            }
+        }
+
+        int best = 0;
+
+        for (int i = 1; i + 1 < type.size(); i++) {
+            if (type.get(i) == '1' &&
+                type.get(i - 1) == '0' &&
+                type.get(i + 1) == '0') {
+
+                best = Math.max(best, len.get(i - 1) + len.get(i + 1));
+            }
+        }
+
+        return ones + best;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
