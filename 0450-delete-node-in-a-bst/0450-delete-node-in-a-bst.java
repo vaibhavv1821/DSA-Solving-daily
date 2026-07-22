59653fe99e71894fdcd755fbf3b5df3c03407dd6
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-22 22:32:29 +0530

    .java Medium 0 ms (100%) 07-22-2026

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,48 +1,41 @@
-
 class Solution {
+
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null){
-            return root;
+
+        if (root == null)
+            return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
         }
-        if(key<root.val){
-            root.left = deleteNode(root.left,key);
-        }else if(key>root.val){
-            root.right = deleteNode(root.right,key);
-        }else{
-            //root.val == key
-            //leaf node
-            if(root.left==null && root.right==null){
-                return null;
-            }
-            // single child case
-            if(root.left==null){
+        else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        }
+        else {
+
+            // Case 1 & 2
+            if (root.left == null)
                 return root.right;
-            }else if(root.right==null){
+
+            if (root.right == null)
                 return root.left;
-            }else{
-                //2child case
-                //inorder predecessor
-                // TreeNode node = findPredecessor(root.left);
-                TreeNode node = findSuccessor(root.right);
-                // shift the node
-                root.val = node.val;
-                //delete node.val
-                root.right = deleteNode(root.right,node.val);
-            }
+
+            // Case 3
+            TreeNode successor = findMin(root.right);
+
+            root.val = successor.val;
+
+            root.right = deleteNode(root.right, successor.val);
         }
+
         return root;
     }
-    public TreeNode findSuccessor(TreeNode node){
-        while(node.left!=null){
+
+    private TreeNode findMin(TreeNode node) {
+
+        while (node.left != null)
             node = node.left;
-        }
-        return node;
-    }
-    
-    public TreeNode findPredecessor(TreeNode node){
-        while(node.right!=null){
-            node = node.right;
-        }
+
         return node;
     }
 }
