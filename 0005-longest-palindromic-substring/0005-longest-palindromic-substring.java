c5e9d9b80e76bdeb1147625d7f5d9188647e9622
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-27 18:38:41 +0530

    .java Medium 13 ms (96.28%) 07-27-2026

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    int start = 0;
+    int maxLength = 0;
+
+    public String longestPalindrome(String s) {
+
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // Check odd length palindrome
+            expand(s, i, i);
+
+            // Check even length palindrome
+            expand(s, i, i + 1);
+        }
+
+        return s.substring(start, start + maxLength);
+    }
+
+    private void expand(String s, int left, int right) {
+
+        while (left >= 0 &&
+               right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+
+            left--;
+            right++;
+        }
+
+        int length = right - left - 1;
+
+        if (length > maxLength) {
+            maxLength = length;
+            start = left + 1;
+        }
+    }
+}
