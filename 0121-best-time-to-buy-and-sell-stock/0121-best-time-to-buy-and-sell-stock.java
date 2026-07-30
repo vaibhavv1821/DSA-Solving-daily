cc0a86073a43fd506f92493af181ec4800ef12c8
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-07-30 19:23:28 +0530

    .java Easy 1 ms (99.95%) 07-30-2026

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,18 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
+    public int maxProfit(int[] arr) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        int profit =0;
 
-        for (int price : prices) {
-            if (price < minPrice) {
-                minPrice = price; // update minimum buying price
-            } else if (price - minPrice > maxProfit) {
-                maxProfit = price - minPrice; // update max profit
-            }
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+
+            profit = arr[i] - min;
+
+            max = Math.max(max, profit);
+
         }
-        return maxProfit;
+
+        return max;
     }
 }
