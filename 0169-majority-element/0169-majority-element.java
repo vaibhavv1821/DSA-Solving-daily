307d62775b42bc774b25bd4b2525ac51b5e66842
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-08-02 17:52:15 +0530

    .java Easy 6 ms (51.93%) 08-02-2026

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,23 +1,6 @@
-import java.util.*;
-
 class Solution {
-
     public int majorityElement(int[] arr) {
-
-        int count =1;
-        int output = arr[0];
-
-        for (int i=1; i<arr.length ; i++) {
-            if(count <= 0){
-                // count = 0;
-                output = arr[i];
-            }
-            if(arr[i] == output){
-                count++;
-            }else{
-                count--;
-            }
-        }
-        return output;
+        Arrays.sort(arr);
+        return arr[arr.length / 2];
     }
 }
