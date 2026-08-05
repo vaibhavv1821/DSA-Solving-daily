6c5c848a7bf9ecf65a656068f86893cf2cf9ba86
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-08-05 23:39:40 +0530

    .java Medium 0050-powx-n 0 ms (100%)

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(x==0 || x==1){
+            return x;
+        }
+        double ans = Math.pow(x, n);
+        return ans;
+    }
+}
