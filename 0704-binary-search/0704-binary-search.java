9e278e94ad40154af48e8d37ad1aa26c763ec296
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-08-06 23:27:59 +0530

    .java Easy 0704-binary-search 0 ms (100%)

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,8 +1,18 @@
 class Solution {
     public int search(int[] arr, int target) {
-        for (int i=0;i<arr.length;i++){
-            if (arr[i] == target) {
-                return i;
+        int low = 0;
+        int high = arr.length-1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            else if (target < arr[mid])
+                high = mid - 1;
+            else {
+                low = mid + 1;
             }
         }
         return -1;
