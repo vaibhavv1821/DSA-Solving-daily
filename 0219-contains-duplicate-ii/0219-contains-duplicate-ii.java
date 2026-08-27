91f79ada8a985ee6cf6933cc48953bac7d524002
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-08-27 11:53:12 +0530

    08-27-2026 .java Easy 0219-contains-duplicate-ii 30 ms (34.55%)

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,20 +1,15 @@
-import java.util.HashSet;
-
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashSet<Integer> set = new HashSet<>();
+        HashMap <Integer,Integer> map=new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
-                return true;  // duplicate within window
+        for(int i=0; i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                if(i-map.get(nums[i]) <= k){
+                    return true;
+                }
             }
-            set.add(nums[i]);
-
-            if (set.size() > k) {
-                set.remove(nums[i - k]); // slide the window
-            }
+            map.put(nums[i] , i);
         }
-
         return false;
     }
 }
