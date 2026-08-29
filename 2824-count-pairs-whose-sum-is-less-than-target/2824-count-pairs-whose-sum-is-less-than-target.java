4826a6d0ac2ba1ef4bf0df2bcfeb766b03ab61b4
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-08-30 00:36:36 +0530

    08-30-2026 .java Easy 2824-count-pairs-whose-sum-is-less-than-target 3 ms (45%)

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,14 +1,24 @@
 class Solution {
     public int countPairs(List<Integer> arr, int target) {
-        int count =0;
-        for(int i=0; i<arr.size();i++){
-            for(int j=i+1;j<arr.size();j++){
-                int sum = arr.get(i) + arr.get(j);
-                if(sum < target){
-                    count++;
-                }
+
+        Collections.sort(arr);
+
+        int count = 0;
+        int i = 0;
+        int j = arr.size() - 1;
+
+        while (i < j) {
+
+            int sum = arr.get(i) + arr.get(j);
+
+            if (sum < target) {
+                count += j - i;
+                i++;
+            } else {
+                j--;
             }
         }
+
         return count;
     }
 }
