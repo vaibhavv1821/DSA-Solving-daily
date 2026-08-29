7bb24e2117db296b9e63d1d56862d30edf4e67df
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-08-29 23:57:18 +0530

    08-29-2026 .java Easy 0088-merge-sorted-array 0 ms (100%)

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,11 +1,10 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int i = m - 1;       // last valid element in nums1
-        int j = n - 1;       // last element in nums2
-        int k = m + n - 1;   // last position of nums1
+        int i = m - 1;      
+        int j = n - 1;      
+        int k = m + n - 1;   
 
-        // Compare from the end and put the larger element at k
         while (i >= 0 && j >= 0) {
 
             if (nums1[i] > nums2[j]) {
@@ -18,8 +17,6 @@ class Solution {
 
             k--;
         }
-
-        // If nums2 still has elements, copy them
         while (j >= 0) {
             nums1[k] = nums2[j];
             j--;
