23cca29046d03fa2a7b9716b2f4427ea06ce6ee5
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-09-06 12:03:16 +0530

    09-06-2026 .java Easy 0724-find-pivot-index 1 ms (97.05%)

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int pivotIndex(int[] arr) {
-        int totalSum = 0;
-        for (int num : arr) {
-            totalSum += num;
-        }
+    public int pivotIndex(int[] nums) {
+        int totalSum=0;
+        int leftSum =0;
 
-        int leftSum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int rightSum = totalSum - leftSum - arr[i];
-            if (leftSum == rightSum) {
-                return i;  // pivot index found
+        for(int n: nums){
+            totalSum+=n;
+        }
+        for(int i=0; i<nums.length;i++){
+            int rightSum = totalSum-leftSum -nums[i];
+
+            if(leftSum==rightSum){
+                return i;
             }
-            leftSum += arr[i];
+            leftSum+=nums[i];
         }
-
-        return -1;  // no pivot index exists
+        return -1;
     }
 }
