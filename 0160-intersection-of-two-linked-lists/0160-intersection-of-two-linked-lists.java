8b2c9dc480a14fc59e3170cdb0799ef8dc2a31fe
Vaibhav Gatlewar <gatlewarvaibhav@gmail.com>
2026-09-23 10:47:09 +0530

    09-23-2026 .java Easy 0160-intersection-of-two-linked-lists 1 ms (99.93%)

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -1,20 +1,24 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
         ListNode first = headA;
         ListNode sec = headB;
 
-        HashSet <ListNode> set =new HashSet<>();
+        while (first != sec) {
 
-        while(first!=null){
-            set.add(first);
-            first = first.next;
-        } 
-        while(sec != null){
-            if(set.contains(sec)){
-                return sec;
+            if (first == null) {
+                first = headB;
+            } else {
+                first = first.next;
             }
-            sec = sec.next;
+
+            if (sec == null) {
+                sec = headA;
+            } else {
+                sec = sec.next;
+            }
         }
-        return null;
+
+        return first;
     }
 }
